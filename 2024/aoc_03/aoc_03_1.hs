814a7681b133a8308c2a9cf657b71de4e60d9590
Alexandre Berard <alexandre.berard2@univ-grenoble-alpes.fr>
2024-12-04 18:52:04 +0100

    day 3

---
+++ 2024/aoc_03/aoc_03_1.hs
@@ -0,0 +1,21 @@
+#!/usr/bin/env cabal
+{- cabal:
+ build-depends: base, regex-tdfa
+-}
+
+import System.Environment (getArgs)
+import Text.Regex.TDFA
+
+parse path = do readFile path
+
+f :: String -> Int
+f l =
+  let mulRegex = "mul\\(([0-9]+),([0-9]+)\\)" in
+  case l =~ mulRegex :: (String, String, String, [String]) of
+  (_, _, n, [a, b]) -> read a * read b + f n
+  _ -> 0
+
+main = do
+  (a : _) <- getArgs
+  l <- parse a
+  print (f l)
---
+++ 2024/aoc_03/aoc_03_2.hs
@@ -0,0 +1,23 @@
+#!/usr/bin/env cabal
+{- cabal:
+ build-depends: base, regex-tdfa
+-}
+
+import System.Environment (getArgs)
+import Text.Regex.TDFA
+
+parse path = do readFile path
+
+f :: String -> Bool -> Int
+f l t =
+  let mulRegex = "mul\\(([0-9]+),([0-9]+)\\)|(do(n't)?\\(\\))" in
+  case l =~ mulRegex :: (String, String, String, [String]) of
+  (_, _, n, [_, _, "do()", _]) -> f n True
+  (_, _, n, [_, _, "don't()", _]) -> f n False
+  (_, _, n, [a, b, _, _]) -> (if t then read a * read b else 0) + f n t
+  _ -> 0
+
+main = do
+  (a : _) <- getArgs
+  l <- parse a
+  print (f l True)
