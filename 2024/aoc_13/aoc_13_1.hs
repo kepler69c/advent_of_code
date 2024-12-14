6fcfd9824270418a0c32a8531360ecd2d5fe1db6
Alexandre Berard <alexandre.berard2@univ-grenoble-alpes.fr>
2024-12-14 08:32:54 +0100

    refactoring 13 again

--- 2024/aoc_13/aoc_13_1.hs
+++ 2024/aoc_13/aoc_13_1.hs
@@ -1,7 +1,6 @@
 import System.Environment (getArgs)
 import Data.List.Split
 
-parse :: FilePath -> IO [((Double, Double), (Double, Double), (Double, Double))]
 parse path = do
   c <- readFile path
   let l = lines c
@@ -19,29 +18,26 @@ parse path = do
         ((ax, ay), (bx, by), (px, py))) ll
   return rl
 
-vec3_mul a [v1, v2, v3] =
-  [a * v1, a * v2, a * v3]
+vecMul a = map (* a)
 
-vec3_sub [v1, v2, v3] [v4, v5, v6] =
-  [v1 - v4, v2 - v5, v3 - v6]
+vecSub = zipWith (-)
 
 solve [[m11, m12], [m21, m22]] [p1, p2] =
-  let [m21_1, m22_1, p2_1] = vec3_sub [m21, m22, p2] (vec3_mul (m21 / m11) [m11, m12, p1])
-      [m11_2, m12_2, p1_2] = vec3_sub [m11, m12, p1] (vec3_mul (m12 / m22_1) [m21_1, m22_1, p2_1])
-      [m11_3, m12_3, p1_3] = vec3_mul (1/m11_2) [m11_2, m12_2, p1_2]
-      [m21_4, m22_4, p2_4] = vec3_mul (1/m22_1) [m21_1, m22_1, p2_1] in
-  [p1_3, p2_4]
+  let [m211, m221, p21] = vecSub [m21, m22, p2] (vecMul (m21 / m11) [m11, m12, p1])
+      [m112, m122, p12] = vecSub [m11, m12, p1] (vecMul (m12 / m221) [m211, m221, p21])
+      [m113, m123, p13] = vecMul (1 / m112) [m112, m122, p12]
+      [m214, m224, p24] = vecMul (1 / m221) [m211, m221, p21] in
+  [p13, p24]
 
 check l =
   let [a, b, ax, ay, bx, by, px, py] = map round l in
   (a * ax + b * bx, a * ay + b * by) == (px, py)
 
-f :: [((Double, Double), (Double, Double), (Double, Double))] -> Int
-f = sum . map (\((ax, ay), (bx, by), (px, py)) ->
-            let [a, b] = solve [[ax, bx], [ay, by]] [px, py] in
-            if check [a, b, ax, ay, bx, by, px, py] then
-              3 * round a + round b
-            else 0)
+f = sum .
+    map (\((ax, ay), (bx, by), (px, py)) ->
+      let [a, b] = solve [[ax, bx], [ay, by]] [px, py] in
+      if check [a, b, ax, ay, bx, by, px, py] then 3 * round a + round b
+      else 0)
 
 main = do
   (a : _) <- getArgs
