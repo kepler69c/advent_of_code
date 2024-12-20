89724a8b5f168f5b0660ce1c7a2357768d3c5192
Alexandre Berard <alexandre.berard2@univ-grenoble-alpes.fr>
2024-12-20 14:10:30 +0100

    day 19

---
+++ 2024/aoc_19/aoc_19_1.hs
@@ -0,0 +1,28 @@
+import Data.List.Split (splitOn)
+import System.Environment (getArgs)
+import Data.Map (Map)
+import qualified Data.Map as M
+
+parse path = do
+  c <- readFile path
+  let a : _ : l = lines c
+      t = splitOn ", " a
+  return (t, l)
+
+f (towels, styles) =
+  length $ filter id $ map (snd . search M.empty towels) styles
+ where
+  splits l = map (`splitAt` l) [1..length l]
+  search mem towels [] = (mem, True)
+  search mem towels style =
+    case mem M.!? style of
+    Just e -> (mem, e)
+    Nothing ->
+      foldr (\(_, e) (mem, x) ->
+        let (mem', x') = search mem towels e in
+        (M.insert e x' mem', x || x')) (mem, False) (filter ((`elem` towels) . fst) (splits style))
+
+main = do
+  (a : _) <- getArgs
+  l <- parse a
+  print (f l)
